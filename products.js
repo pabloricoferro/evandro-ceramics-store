c823f217e9e121bd64d2a5c763da12da01d09b30
Pablo Rico Ferro <pabloricoferro@gmail.com>
2026-04-22 23:25:24 +0200

    Create initial Evandro ceramics storefront
    Set up a blue-themed catalog with product data in a dedicated file and dynamic filters by type and artist to support future expansion.

---
+++ products.js
@@ -0,0 +1,29 @@
+window.PRODUCTS = [
+  {
+    id: "eg-wall-sequence-01",
+    title: "Wall Sequence I",
+    artist: "Evandro Gabrieli",
+    type: "Wall Piece",
+    priceEUR: 1800,
+    image: "./images/wall-sequence-1.png",
+    alt: "Series of vertical ceramic wall pieces",
+  },
+  {
+    id: "eg-vessel-01",
+    title: "Blue Rust Vessel",
+    artist: "Evandro Gabrieli",
+    type: "Vessel",
+    priceEUR: 640,
+    image: "./images/blue-rust-vessel.png",
+    alt: "Textured ceramic vessel in blue and earthy tones",
+  },
+  {
+    id: "eg-signature-01",
+    title: "Studio Signature Mark",
+    artist: "Evandro Gabrieli",
+    type: "Limited Design",
+    priceEUR: 320,
+    image: "./images/studio-signature.png",
+    alt: "Gabrieli Evandro gallery and studio signature",
+  },
+];
