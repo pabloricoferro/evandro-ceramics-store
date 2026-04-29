28471c1e486307e34bdfb5367742bebf3209f1b4
Pablo Rico Ferro <pabloricoferro@gmail.com>
2026-04-29 23:12:14 +0200

    & "C:\Program Files\Git\bin\git.exe" push origin mainRestore initial Clay & Hearth storefront version

---
+++ script.js
@@ -0,0 +1,23 @@
+const cartCountEl = document.getElementById("cartCount");
+const yearEl = document.getElementById("year");
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+let cartCount = 0;
+
+addToCartButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    cartCount += 1;
+    cartCountEl.textContent = String(cartCount);
+
+    const originalText = button.textContent;
+    button.textContent = "Added!";
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.textContent = originalText;
+      button.disabled = false;
+    }, 700);
+  });
+});
+
+yearEl.textContent = String(new Date().getFullYear());
